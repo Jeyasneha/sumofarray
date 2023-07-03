f08def0aaf155fd82b72c0cbcdc3f8178529ce67
22mx112 <22mx112@MCA.PSG>
2023-07-03 14:12:44 +0530

    first commit of sumofarray

---
+++ sum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class sum{
+    public static void main(String[] args)
+    {
+        int total=0;
+        System.out.println("Enter the size of an array");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter array elements:");
+        for(int i=0;i<size;i++)
+        {
+            arr[i] = sc.nextInt();
+            total = total + arr[i];
+            
+        
+
+        }
+        System.out.println("Sum of the elements of an array:" +total);
+
+    }
+}
